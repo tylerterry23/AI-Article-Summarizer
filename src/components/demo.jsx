414a649484dafafc7f683547e0224efd717ab0f8
Tyler Terry <tvp7098@wmich.edu>
2023-07-09 19:34:22 -0400

    API endpoint connection and demo link accept/pass

--- src/components/demo.jsx
+++ src/components/demo.jsx
@@ -1,8 +1,75 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+
+import { copy, linkIcon, loader, tick } from '../assets'
+import { useLazyGetSummaryQuery } from '../services/article'
 
 const demo = () => {
+
+  const [article, setArticle] = useState({
+    url: '',
+    summary: '',
+  })
+
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery()
+
+  const handleSubmit = async (e) => {
+
+    e.preventDefault()
+
+    // API call
+    const { data } = await getSummary({ articleUrl: article.url })
+
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary }
+
+      setArticle(newArticle)
+
+      console.log(newArticle)
+    } else {
+      console.log('error', error)
+    }
+  }
+
   return (
-    <div>demo</div>
+    <section className='mt-16 w-full max-w-xl'>
+      
+      {/* Search */}
+      <div className='flex flex-col w-full gap-2'>
+        <form 
+          className='relative flex justify-center items-center'
+          onSubmit={handleSubmit}
+        >
+          <img 
+            src={linkIcon}
+            alt="link icon"
+            className='absolute left-0 my-2 ml-3 w-5' 
+          />
+
+          <input 
+            type="url" 
+            placeholder='enter URL'
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            required
+            className='url_input peer'
+          />
+
+          <button
+            type='submit'
+            className='submit_btn 
+            peer-focus:border-gray-700 
+            peer-focus:text-gray-700'
+          >
+            ⤶
+          </button>
+        </form>
+
+        {/* Browser URL History */}   
+      </div>
+
+      {/* Display Results */}
+
+    </section>
   )
 }
 
