cfc91e1f806a3cea986424e096074b1aae0721ff
Tyler Terry <tvp7098@wmich.edu>
2023-07-09 21:45:15 -0400

    deployment trouble shooting

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 
-import App from "./App.jsx";
+import App from "./app.jsx";
 import { store } from "./services/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
